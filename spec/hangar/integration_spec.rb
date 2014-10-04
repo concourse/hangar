de5d814f58a07a87c4885bd580c42989fe459ab9
Chris Brown <xoebus@xoeb.us>
2014-10-04 20:08:53 +0100

    dedup args so that new ones do not require shotgun changes

--- spec/hangar/integration_spec.rb
+++ spec/hangar/integration_spec.rb
@@ -20,10 +20,28 @@ describe 'Hangar' do
     entries
   end
 
+  def args(arg_hash)
+    output = []
+
+    arg_hash.each do |key, value|
+      output << "--#{key} #{value}"
+    end
+
+    output.join(' ')
+  end
+
   describe 'building .pivotal files' do
     let(:stemcell_dir) { 'spec/assets/stemcell' }
     let(:release_dir) { 'spec/assets/release' }
     let(:product_name) { 'p-product' }
+
+    let(:valid_args) {
+      {
+          'product-name' => product_name,
+          'stemcell-dir' => stemcell_dir,
+          'release-dir' => release_dir,
+      }
+    }
 
     before do
       FileUtils.rm_rf('p-product.pivotal')
@@ -31,40 +49,48 @@ describe 'Hangar' do
 
     it 'creates a .pivotal file' do
       expect {
-        hangar("--stemcell-dir #{stemcell_dir} --release-dir #{release_dir} --product-name #{product_name}")
+        hangar(args(valid_args))
       }.to change { File.exist? 'p-product.pivotal' }.from(false).to(true)
     end
 
     it 'creates a zip file with the correct stemcell' do
-      hangar("--stemcell-dir #{stemcell_dir} --release-dir #{release_dir} --product-name #{product_name}")
+      hangar(args(valid_args))
 
       expect(files_in('p-product.pivotal')).to include('stemcells/stemcell.tgz')
     end
 
     it 'creates a zip file with the correct release' do
-      hangar("--stemcell-dir #{stemcell_dir} --release-dir #{release_dir} --product-name #{product_name}")
+      hangar(args(valid_args))
 
       expect(files_in('p-product.pivotal')).to include('releases/release.tgz')
     end
 
     context 'with missing args' do
       it 'returns an error if no product name is given' do
+        missing_product_name = valid_args.reject { |k,v| k == 'product-name' }
+
         expect {
-          hangar("--stemcell-dir #{stemcell_dir} --release-dir a/missing/dir")
+          hangar(args(missing_product_name))
         }.to raise_error /Please specify a product name \(--product-name\)/
       end
     end
 
     context 'with missing resources' do
       it 'returns an error if no stemcell could be found' do
+        bad_stemcell = valid_args.dup
+        bad_stemcell.store('stemcell-dir', 'a/missing/dir')
+
         expect {
-          hangar("--stemcell-dir a/missing/dir --release-dir #{release_dir} --product-name #{product_name}")
+          hangar(args(bad_stemcell))
         }.to raise_error /Could not find a stemcell in directory: a\/missing\/dir/
       end
 
       it 'returns an error if no release could be found' do
+        bad_release = valid_args.dup
+        bad_release.store('release-dir', 'a/missing/dir')
+
         expect {
-          hangar("--stemcell-dir #{stemcell_dir} --release-dir a/missing/dir --product-name #{product_name}")
+          hangar(args(bad_release))
         }.to raise_error /Could not find a release in directory: a\/missing\/dir/
       end
     end
