18f1108a7138d223702f5a0ff57a5c3ddde070fc
Rob Dimsdale <robert.dimsdale@gmail.com>
2015-05-12 18:02:15 -0700

    Remove references to stemcell
    - OpsMgr products no longer need to ship with a stemcell

--- spec/hangar/metadata_spec.rb
+++ spec/hangar/metadata_spec.rb
@@ -4,24 +4,15 @@ require 'yaml'
 
 require 'hangar/metadata_template'
 require 'hangar/release'
-require 'hangar/stemcell'
 
 describe Hangar::MetadataTemplate do
   describe 'templating the metadata' do
+    let(:result) { template.result(product_name, product_version, releases) }
     let(:template) { Hangar::MetadataTemplate.from_file('spec/assets/metadata/metadata.yml.erb') }
-    let(:result) { template.result(product_name, product_version, stemcell, releases) }
     let(:output) { YAML.load(result) }
 
     let(:product_name) { 'p-product' }
     let(:product_version) { '1.0.0.0' }
-    let(:stemcell) {
-      instance_double(
-          Hangar::Stemcell,
-          name: 'bosh-vsphere-esxi-ubuntu-trusty-go_agent',
-          version: '2690.3',
-          file: 'bosh-stemcell-2690.3-vsphere-esxi-ubuntu-trusty-go_agent.tgz',
-      )
-    }
     let(:releases) {
       [
           instance_double(
@@ -39,12 +30,6 @@ describe Hangar::MetadataTemplate do
 
     it 'templates the product version' do
       expect(output.fetch('product_version')).to eq(product_version)
-    end
-
-    it 'templates in the stemcell' do
-      expect(output.fetch('stemcell').fetch('name')).to eq(stemcell.name)
-      expect(output.fetch('stemcell').fetch('version')).to eq(stemcell.version)
-      expect(output.fetch('stemcell').fetch('file')).to eq(stemcell.file)
     end
 
     it 'templates the releases' do
